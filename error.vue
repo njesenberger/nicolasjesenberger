19125dce46e1140a19e35d5a0611f9f2fd94e723
Nicolas Jesenberger <nicolas@MacBook-Air-de-Nicolas.local>
2023-10-22 00:13:49 +0200

    add: 404 page

---
+++ error.vue
@@ -0,0 +1,12 @@
+<template>
+	<NuxtLayout name="error">
+		<Heading class="heading-1">404 — Page not found</Heading>
+		<Link to="/">Go back to homepage</Link>
+	</NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+useHead({
+	title: '404 — Page not found',
+});
+</script>
---
+++ layouts/error.vue
@@ -0,0 +1,29 @@
+<template>
+	<slot></slot>
+</template>
+
+<style lang="scss">
+:root {
+	color-scheme: dark;
+}
+
+body {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin: 0 auto;
+	padding: 0 24px;
+	max-width: 35em;
+	min-height: 100vh;
+	min-height: 100dvh;
+	background-color: var(--neutral-color-5);
+	text-align: center;
+	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Open Sans', system-ui, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji';
+	color: var(--neutral-color-2);
+}
+
+::selection {
+	background-color: #0004bc;
+	color: #fff;
+}
+</style>
